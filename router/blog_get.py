96944b7972c326aa2a6033db304bdf161fe59d2f
Florek <pawel.florczak87@gmail.com>
2025-12-02 22:13:28 +0100

    02.12.2025

---
+++ router/blog_get.py
@@ -0,0 +1,39 @@
+from typing import Optional
+
+from fastapi import APIRouter, status, Response
+
+router = APIRouter(
+    prefix='/blog',
+    tags=['blog']
+)
+
+@router.get(
+    '/all',  
+    summary='Retrieve all blogs',
+    description='This api call simulates fetching all blogs'
+)
+def get_blogs(page = 1, page_size: Optional[int] = None):
+    return {'message': f'All {page_size} blogs on page {page}'}
+
+
+@router.get('/{id}/comments/{comment_id}', tags=['comment'])
+def get_comments(id: int, comment_id: int, valid: bool = True, username: Optional[str] = None):
+    """
+    Simulates retrieving a comment of a blog
+
+    - **id** mandatory path parameter
+    - **comment_id** mandatory path parameter
+    - **valid** optional query parameter
+    - **username** optional query parameter
+    """
+    return {'message': f'blog {id}, comment_id: {comment_id}, valid: {valid}, username: {username}'}
+
+
+@router.get('/{id}', status_code=status.HTTP_200_OK)
+def get_blog(id: int, response: Response):
+    if id > 5:
+        response.status_code = status.HTTP_404_NOT_FOUND
+        return {'error': f'Blog {id} not found'}
+    else:
+        return {'message': f'Blog with id {id}'}
+
