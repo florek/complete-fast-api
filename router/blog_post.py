faaad13f2026e08026e17bf6bb3c6b882a86e498
Florek <pawel.florczak87@gmail.com>
2025-12-05 18:44:06 +0100

    05.12.2025

--- router/blog_post.py
+++ router/blog_post.py
@@ -1,6 +1,6 @@
 from typing import Optional
 
-from fastapi import APIRouter
+from fastapi import APIRouter, Query, Path, Body
 from pydantic import BaseModel
 
 
@@ -22,4 +22,22 @@ def create_blog(blog: BlogModel, id: int, version: int = 1):
         'id': id,
         'data': blog,
         'version': version
+    }
+
+@router.post('/new/{id}/comment')
+def create_comment(
+    blog: BlogModel,
+    id: int,
+    comment_id: int = Query(
+        None,
+        title='Id of the comment',
+        description='Some description for comment_id',
+        alias='commentId',
+        deprecated=True
+    )
+):
+    return {
+        'blog': blog,
+        'id': id,
+        'comment_id': comment_id
     }
